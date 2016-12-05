c805b4d9c5280fde04b277ecb9e1cc701ffff753
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-12-05 16:33:49 +0100

    .NET ResX support added

--- src/ResxProcessor.cs
+++ src/ResxProcessor.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +11,43 @@ namespace PseudoInternationalization {
 
     class ResxProcessor : BaseProcessor {
 
+        List<DictionaryEntry> _resources;
+
         protected override void Load(Stream input) {
-            throw new NotImplementedException();
+            _resources = new List<DictionaryEntry>();
+
+            using (var r = new ResXResourceReader(input)) {
+                foreach(DictionaryEntry entry in r) {
+                    if (entry.Value == null)
+                        continue;
+
+                    if (!(entry.Value is string))
+                        continue;
+
+                    var key = entry.Key.ToString();
+                    if(
+                        key.StartsWith(">>") ||
+                        (key.StartsWith("$") && !key.Equals("$this.Text", StringComparison.InvariantCultureIgnoreCase))
+                    ) {
+                        // Make sure the key does not start with meta-characters, excluding
+                        // "$this.Text" for Form titles.
+                        continue;
+                    }
+
+                    _resources.Add(entry);
+                }
+            }
         }
 
         protected override void Save(Stream output) {
-            throw new NotImplementedException();
+            using(var w = new ResXResourceWriter(output)) {
+                foreach(var entry in _resources) {
+                    w.AddResource(
+                        entry.Key.ToString(),
+                        Translator.ConvertToFakeInternationalized((string)entry.Value)
+                    );
+                }
+            }
         }
 
     }
